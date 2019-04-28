b2c1c4815ed1bca6cf97525260c9e12cf82ec821
Gabriel Féron <gabriel.feron@here.com>
2019-04-28 13:31:47 +0200

    Add LICENSE-* and README

---
+++ README.md
@@ -0,0 +1,23 @@
+# am2320
+
+A platform-agnostic driver to interface with the AM2320 I2c temperature & humidity sensor.
+
+## Examples
+
+You can find an example to use the sensor with a RaspberryPI under `examples/`.
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the
+work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any
+additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,26 @@ pub struct AM2320<I2C, Delay> {
     delay: Delay,
 }
 
+fn crc16(data: &[u8]) -> u16 {
+    let mut crc: u16 = 0xFFFF;
+    for e in data.iter() {
+        crc ^= *e as u16;
+        for _i in 0..8 {
+            if (crc & 0x0001) == 0x0001 {
+                crc >>= 1;
+                crc ^= 0xA001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    crc
+}
+
+fn combine_bytes(msb: u8, lsb: u8) -> u16 {
+    ((msb as u16) << 8) | lsb as u16
+}
+
 impl<I2C, Delay, E> AM2320<I2C, Delay>
 where
     I2C: i2c::Read<Error = E> + i2c::Write<Error = E>,
@@ -29,32 +49,12 @@ where
         Self { device, delay }
     }
 
-    fn crc16(data: &[u8]) -> u16 {
-        let mut crc: u16 = 0xFFFF;
-        for e in data.iter() {
-            crc ^= *e as u16;
-            for _i in 0..8 {
-                if (crc & 0x0001) == 0x0001 {
-                    crc >>= 1;
-                    crc ^= 0xA001;
-                } else {
-                    crc >>= 1;
-                }
-            }
-        }
-        crc
-    }
-
-    fn combine_bytes(msb: u8, lsb: u8) -> u16 {
-        ((msb as u16) << 8) | lsb as u16
-    }
-
     pub fn read(&mut self) -> Result<Measurement, Error> {
         // wake the AM2320 up, goes to sleep to not warm up and affect the humidity sensor
         // this write will fail as AM2320 won't ACK this write
         self.device
             .write(DEVICE_I2C_ADDR, &[0x00])
-            .map_err(|e| Error::ReadError)?;
+            .map_err(|_| Error::ReadError)?;
         // wait at least 0.8ms, at most 3ms
         self.delay.delay_us(1000);
 
@@ -62,7 +62,7 @@ where
         // wait at least 1.5ms for the result
         self.device
             .write(DEVICE_I2C_ADDR, &[0x03, 0x00, 0x04])
-            .map_err(|e| Error::ReadError)?;
+            .map_err(|_| Error::ReadError)?;
         self.delay.delay_us(1600);
 
         // read out 8 bytes of result data
@@ -77,7 +77,7 @@ where
         let mut data: Vec<u8> = vec![0; 8];
         self.device
             .read(DEVICE_I2C_ADDR, &mut data)
-            .map_err(|e| Error::ReadError)?;
+            .map_err(|_| Error::ReadError)?;
 
         // check that the operation was reported as succesful
         if data[0] != 0x03 || data[1] != 0x04 {
@@ -85,18 +85,18 @@ where
         }
 
         // CRC check
-        let crc = Self::crc16(&data[0..6]);
-        if crc != Self::combine_bytes(data[7], data[6]) {
+        let crc = crc16(&data[0..6]);
+        if crc != combine_bytes(data[7], data[6]) {
             return Err(Error::SensorError);
         }
 
-        let temperature = Self::combine_bytes(data[4], data[5]);
+        let temperature = combine_bytes(data[4], data[5]);
         // TODO: fix this
         // if the highest bit is 1, the temperature is negative
         // if temperature & 0x8000 == temperature {
         //     temperature = -(temperature & 0x7FFF)
         // }
-        let humidity = Self::combine_bytes(data[2], data[3]);
+        let humidity = combine_bytes(data[2], data[3]);
 
         Ok(Measurement {
             temperature: temperature as f64 / 10.0,
@@ -106,13 +106,13 @@ where
 }
 
 #[test]
-fn crc16() {
-    assert_eq!(AM2320::crc16(&[]), 0xFFFF);
-    assert_eq!(AM2320::crc16(&[0x03, 0x04, 0x02, 0x36, 0x0, 0xDB]), 0x0550);
+fn test_crc16() {
+    assert_eq!(crc16(&[]), 0xFFFF);
+    assert_eq!(crc16(&[0x03, 0x04, 0x02, 0x36, 0x0, 0xDB]), 0x0550);
 }
 
 #[test]
-fn combine_bytes() {
-    assert_eq!(AM2320::combine_bytes(0, 0), 0);
-    assert_eq!(AM2320::combine_bytes(0xC5, 0x01), 0xDB);
+fn test_combine_bytes() {
+    assert_eq!(combine_bytes(0, 0), 0);
+    assert_eq!(combine_bytes(0xC5, 0x01), 0xDB);
 }
