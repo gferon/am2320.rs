a6af79b1ef3edeedf9bd6e765e92c855211618b7
Gabriel Féron <gabriel.feron@here.com>
2019-04-28 13:25:12 +0200

    It works!

--- examples/tempread.rs
+++
@@ -1,6 +0,0 @@
-use am2320::*;
-
-fn main() -> Result<(), CommunicationError> {
-    println!("{:?}", AM2320::read());
-    Ok(())
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,34 @@
-use rppal::i2c::{self, I2c};
+use embedded_hal::blocking::{delay, i2c};
 
-use std::{thread, time};
+const DEVICE_I2C_ADDR: u8 = 0x5c;
 
 #[derive(Debug)]
-pub enum CommunicationError {
-    ReadingError,
-    BusError(i2c::Error),
+pub enum Error {
+    ReadError,
+    WriteError,
+    SensorError,
 }
 
 #[derive(Debug)]
-pub struct AM2320 {
+pub struct Measurement {
     pub temperature: f64,
     pub humidity: f64,
 }
 
-impl AM2320 {
+pub struct AM2320<I2C, Delay> {
+    device: I2C,
+    delay: Delay,
+}
+
+impl<I2C, Delay, E> AM2320<I2C, Delay>
+where
+    I2C: i2c::Read<Error = E> + i2c::Write<Error = E>,
+    Delay: delay::DelayUs<u16>,
+{
+    pub fn new(device: I2C, delay: Delay) -> Self {
+        Self { device, delay }
+    }
+
     fn crc16(data: &[u8]) -> u16 {
         let mut crc: u16 = 0xFFFF;
         for e in data.iter() {
@@ -35,55 +49,56 @@ impl AM2320 {
         ((msb as u16) << 8) | lsb as u16
     }
 
-    pub fn read() -> Result<Self, CommunicationError> {
-        let mut device = I2c::new().map_err(|e| CommunicationError::BusError(e))?;
-        device
-            .set_slave_address(0x5c as u16)
-            .map_err(|e| return CommunicationError::BusError(e))?;
-
-        // wake AM2320 up, goes to sleep to not warm up and affect the humidity sensor
-        // This write will fail as AM2320 won't ACK this write
-        // Wait at least 0.8ms, at most 3ms
-        device.write(&[0x00]).unwrap_or(0);
-        thread::sleep(time::Duration::from_micros(1000));
+    pub fn read(&mut self) -> Result<Measurement, Error> {
+        // wake the AM2320 up, goes to sleep to not warm up and affect the humidity sensor
+        // this write will fail as AM2320 won't ACK this write
+        self.device
+            .write(DEVICE_I2C_ADDR, &[0x00])
+            .map_err(|e| Error::ReadError)?;
+        // wait at least 0.8ms, at most 3ms
+        self.delay.delay_us(1000);
 
         // send command
-        // Wait at least 1.5ms for result
-        device
-            .write(&[0x03, 0x00, 0x04])
-            .map_err(|_| CommunicationError::ReadingError)?;
-        thread::sleep(time::Duration::from_micros(1600));
+        // wait at least 1.5ms for the result
+        self.device
+            .write(DEVICE_I2C_ADDR, &[0x03, 0x00, 0x04])
+            .map_err(|e| Error::ReadError)?;
+        self.delay.delay_us(1600);
 
-        // # Read out 8 bytes of result data
-        // # Byte 0: Should be Modbus function code 0x03
-        // # Byte 1: Should be number of registers to read (0x04)
-        // # Byte 2: Humidity msb
-        // # Byte 3: Humidity lsb
-        // # Byte 4: Temperature msb
-        // # Byte 5: Temperature lsb
-        // # Byte 6: CRC lsb byte
-        // # Byte 7: CRC msb byte
+        // read out 8 bytes of result data
+        // byte 0: Should be Modbus function code 0x03
+        // byte 1: Should be number of registers to read (0x04)
+        // byte 2: Humidity msb
+        // byte 3: Humidity lsb
+        // byte 4: Temperature msb
+        // byte 5: Temperature lsb
+        // byte 6: CRC lsb byte
+        // byte 7: CRC msb byte
         let mut data: Vec<u8> = vec![0; 8];
-        device.read(&mut data).unwrap();
+        self.device
+            .read(DEVICE_I2C_ADDR, &mut data)
+            .map_err(|e| Error::ReadError)?;
 
+        // check that the operation was reported as succesful
         if data[0] != 0x03 || data[1] != 0x04 {
-            return Err(CommunicationError::ReadingError);
+            return Err(Error::SensorError);
         }
 
         // CRC check
         let crc = Self::crc16(&data[0..6]);
         if crc != Self::combine_bytes(data[7], data[6]) {
-            return Err(CommunicationError::ReadingError);
+            return Err(Error::SensorError);
         }
 
         let temperature = Self::combine_bytes(data[4], data[5]);
+        // TODO: fix this
         // if the highest bit is 1, the temperature is negative
         // if temperature & 0x8000 == temperature {
         //     temperature = -(temperature & 0x7FFF)
         // }
         let humidity = Self::combine_bytes(data[2], data[3]);
 
-        Ok(AM2320 {
+        Ok(Measurement {
             temperature: temperature as f64 / 10.0,
             humidity: humidity as f64 / 10.0,
         })
@@ -99,5 +114,5 @@ fn crc16() {
 #[test]
 fn combine_bytes() {
     assert_eq!(AM2320::combine_bytes(0, 0), 0);
-    // assert_eq!(AM2320::combine_bytes(0xC5, 0x01), 0xDB);
+    assert_eq!(AM2320::combine_bytes(0xC5, 0x01), 0xDB);
 }
